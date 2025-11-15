7167849964b31064ccd3a08d5c87cb9156763a34
AwishkarAnant <awishkara15@gmail.com>
2025-11-15 17:26:28 +0530

    recommit revstringword

--- ReverseStringWord.java
+++ ReverseStringWord.java
@@ -1,15 +1,19 @@
 public class ReverseStringWord{
     public static void main(String[] args){
-        String statement = "I Love Akanksha";
+        String statement = "Unlimited Love Happiness";
         System.out.println(statement);
         String[] str = statement.split(" ");
         String rev = "";
 
-        for(int i = str.length-1; i <= 0; i--){
-            rev = rev + str[i];
+        for(int i = str.length-1; i >= 0; i--){
+            rev = rev + str[i]+" ";
         }
 
         System.out.println(rev);
+
+        //Output:
+        // Unlimited Love Happiness
+        // Happiness Love Unlimited
 
     }
 }
